4b165313ce1a9bf763c65e2728a4220862a91f9d
Kevin Stradtman <stradtkt22@gmail.com>
2021-05-13 17:27:46 -0400

    Added some templates

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from flaskext.mysql import MySQL
 
 app = Flask(__name__)
+mysql = MySQL()
+mysql.init_app(app)
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_DB'] = 'rental_db'
+app.config['MYSQL_DATABASE_CHARSET'] = 'utf-8'
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/', methods=('GET',))
+def index():
+    header = {
+        "img_url": "assets/img/bg8.jpg",
+        "title": "Home At Last",
+        "subtitle": "Welcome to Home At Last!  Where you can find your next home rental with ease!"
+    }
+    return render_template("index.html", header=header)
 
 
 if __name__ == '__main__':
